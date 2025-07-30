a99e1480ebfe553c9cf8323dcf5352d955c0867a
neuronovaiofficial <neuronovainfo@gmail.com>
2025-07-31 00:13:50 +0300

    Cloudinary entegrasyonu, template ve model güncellemeleri

--- academic/models.py
+++ academic/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from cloudinary.models import CloudinaryField  # 🔸 bunu ekle
 
+# models.py
+
 class Card(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    image = CloudinaryField('image')  # 🔸 bu satırı değiştirdik
+    image = CloudinaryField('image', blank=True, null=True)
     link = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)  # 🆕 bu satırı ekle
 
     def __str__(self):
         return self.title
+
--- academic/views.py
+++ academic/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from .models import Card
 
 def cards(request):
-    all_cards = Card.objects.all()
+    all_cards = Card.objects.all().order_by('-created_at')  # 🟢 Sıralama eklendi
     return render(request, 'academic/cards.html', {'cards': all_cards})
+
